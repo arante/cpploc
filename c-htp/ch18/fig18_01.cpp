76f67160c7fc870068430cf4f61c896f492fd782
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-18 14:23:08 +0800

    feat: new file fig18_01.cpp
    My first C++ program.

---
+++ c-htp/ch18/fig18_01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/* fig18_01.cpp
+ * Addition of two integers.
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last updated on 2017/12/18 PHT
+ */
+
+int main() {
+        int number1;
+        std::cout << "Enter first integer: ";
+        std::cin >> number1;
+
+        int number2;
+        std::cout << "Enter second integer: ";
+        std::cin >> number2;
+
+        int sum;
+        sum = number1 + number2;
+
+        std::cout << "Sum is " << sum << std::endl;
+
+        return 0;
+}
