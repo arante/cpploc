d7fb8135071db0f32c32a445eef53177f94b5bb7
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-30 17:51:22 +0800

    feat: new file fig02_01.cpp
    Initial commit.

---
+++ cpp-htp/ch02/fig02_01.cpp
@@ -0,0 +1,14 @@
+// fig02_01.cpp
+// Text-printing program.
+//
+// Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+// Last updated on 2017/12/30 PHT
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome to C++ programming!\n";
+
+	return 0;
+}
