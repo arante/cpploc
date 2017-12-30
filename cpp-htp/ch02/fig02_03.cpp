984f1fa1374ed011bd8e03d83c49eda6e500284e
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-30 17:58:11 +0800

    feat: created new file fig02_03.cpp
    Initial commit

---
+++ cpp-htp/ch02/fig02_03.cpp
@@ -0,0 +1,15 @@
+// fig02_03.cpp
+// Printing a line of text with multiple statments.
+//
+// Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+// Last updated on 2017/12/30 PHT
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome to ";
+	std::cout << "C++ programming!\n";
+
+	return 0;
+}
