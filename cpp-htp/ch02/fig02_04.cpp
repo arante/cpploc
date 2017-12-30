de394e57c9a0dffcbd0e41f259cacd59c0ae3cb1
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-30 18:02:52 +0800

    feat: new file fig02_04.cpp
    Initial commit.

---
+++ cpp-htp/ch02/fig02_04.cpp
@@ -0,0 +1,14 @@
+// fig02_04.cpp
+// Printing multiple lines of text with a single statement.
+//
+// Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+// Last updated on 2017/12/30 PHT
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome\nto\n\nC++\nprogramming!\n";
+
+	return 0;
+}
